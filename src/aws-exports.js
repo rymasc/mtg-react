df759e6235b197d6b5462e53fbe4057fd36325cf
Ryan Scott <rscott6@clemson.edu>
2018-10-22 18:06:44 -0400

    Add API Key

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -4,7 +4,7 @@ const awsmobile =  {
     "aws_appsync_graphqlEndpoint": "https://fbzbzjfvwrgvdjbe54glme3mie.appsync-api.us-east-1.amazonaws.com/graphql",
     "aws_appsync_region": "us-east-1",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "null",
+    "aws_appsync_apiKey": "da2-bex5k4iwyzavtdspyakv4itaru",
 };
 
 export default awsmobile;
