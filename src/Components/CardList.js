5bb5b618fa1845d039cc2a334b2ec6f218db9e4e
Ryan Scott <rscott6@clemson.edu>
2018-10-22 16:21:04 -0400

    Remove OG files + Add CardList Component

---
+++ src/Components/CardList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class CardList extends Component {
+  render() {
+    return (
+      <div className="CardList">
+        <ul id="card-list">
+            <li> Card # 1</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default CardList;
